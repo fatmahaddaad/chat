99bc36c99d7a43ab2fa4fdb83dca1499b620c118
Fatma Haddad <haddaadfatma@gmail.com>
2019-03-04 16:30:35 +0100

    added nelmio_cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
